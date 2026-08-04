029190159f9e0bf964c956c43665e38a16c4b051
ExternalMirror <ahmedovveli.outlook.com>
2026-08-04 16:57:13 +0400

    profile update req codded

--- backend/src/main/java/com/twittvl/backend/user/UserUpdateRequest.java
+++ backend/src/main/java/com/twittvl/backend/user/UserUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.twittvl.backend.user;
 
-public record UserUpdateRequest() {
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+
+public record UserUpdateRequest(
+        @Size(max = 160)
+        String bio,
+        @Size(max = 30)
+        String displayName,
+        @URL
+        String avatarURL) {
 }
