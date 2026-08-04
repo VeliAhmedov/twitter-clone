710a21c4e76bb8243a64ae1c932dc1d35f00b649
ExternalMirror <ahmedovveli.outlook.com>
2026-08-04 19:19:08 +0400

    added userMapper

--- backend/src/main/java/com/twittvl/backend/user/UserMapper.java
+++ backend/src/main/java/com/twittvl/backend/user/UserMapper.java
@@ -1,4 +1,14 @@
 package com.twittvl.backend.user;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
 //will use here, mapstruct library
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
+    UserResponse userToUserResponse(User user);
+
+    void applyUpdate(UserUpdateRequest userUpdateRequest, @MappingTarget User user);
 }
