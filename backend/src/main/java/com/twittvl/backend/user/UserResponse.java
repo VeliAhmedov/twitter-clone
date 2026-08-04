3fe383c8a5372e9d407c72b2ad21d421ad5e1b20
ExternalMirror <ahmedovveli.outlook.com>
2026-08-04 16:53:01 +0400

    userResponse codded

--- backend/src/main/java/com/twittvl/backend/user/UserResponse.java
+++ backend/src/main/java/com/twittvl/backend/user/UserResponse.java
@@ -1,5 +1,11 @@
 package com.twittvl.backend.user;
 import java.time.Instant;
 
-public record UserResponse() {
+public record UserResponse(
+        Long id,
+        String email,
+        String username,
+        String displayName,
+        String avatarURL,
+        Instant createdAdd) {
 }
