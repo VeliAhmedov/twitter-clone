dcfb34c4fe81c8273328bb889a0f7f77d48b3cc9
ExternalMirror <ahmedovveli.outlook.com>
2026-08-20 21:19:33 +0400

    tweet repo

--- backend/src/main/java/com/twittvl/backend/tweet/TweetRepository.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetRepository.java
@@ -1,4 +1,12 @@
 package com.twittvl.backend.tweet;
 
-public interface TweetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+    Page<Tweet> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+    Page<Tweet> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
