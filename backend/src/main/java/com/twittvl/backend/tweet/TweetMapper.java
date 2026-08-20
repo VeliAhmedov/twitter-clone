bda87f196cfa3ff85b3bfd69d5873d150d644871
ExternalMirror <ahmedovveli.outlook.com>
2026-08-20 22:52:25 +0400

    tweet mapper

--- backend/src/main/java/com/twittvl/backend/tweet/TweetMapper.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetMapper.java
@@ -1,4 +1,13 @@
 package com.twittvl.backend.tweet;
 
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TweetMapper {
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "userAvatarUrl", source = "user.avatarUrl")
+    TweetResponse tweetToTweetResponse(Tweet tweet);
+
+    void applyUpdate (TweetRequest tweetRequest, @MappingTarget Tweet tweet);
 }
