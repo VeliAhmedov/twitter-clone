2dd95ebfe0cdf3c77e32b41cdc077a320fb63caa
ExternalMirror <ahmedovveli.outlook.com>
2026-08-26 23:37:57 +0400

    Global exception class ready

--- backend/src/main/java/com/twittvl/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/twittvl/backend/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,43 @@
 package com.twittvl.backend.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleNotFound(ResourceNotFoundException rex) {
+        return  buildResponseEntity(HttpStatus.NOT_FOUND, rex.getMessage());
+    }
+
+    public ResponseEntity<ApiErrorResponse> handleBadRequest(IllegalArgumentException rex) {
+        return  buildResponseEntity(HttpStatus.BAD_REQUEST, rex.getMessage());
+    }
+
+    //replace concationation with better performance method
+    public ResponseEntity<ApiErrorResponse> handleValidationError(MethodArgumentNotValidException rex) {
+         String message = rex.getBindingResult().getFieldErrors().stream()
+                 .findFirst()
+                 .map(f -> f.getField() + " : "+ f.getDefaultMessage())
+                 .orElse("Validation failed");
+         return  buildResponseEntity(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleUnexpected(Exception ex) {
+        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong");
+    }
 
 
+    private ResponseEntity<ApiErrorResponse> buildResponseEntity(HttpStatus status, String message) {
+         ApiErrorResponse apiErrorResponse = new ApiErrorResponse(status.value(), message, Instant.now());
+         return  ResponseEntity.status(status).body(apiErrorResponse);
+    }
 }
