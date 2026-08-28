9dff20d6e9ef8fe9fe5d1b476954ff0f0323a978
ExternalMirror <ahmedovveli.outlook.com>
2026-08-28 18:58:14 +0400

    added get feed

--- backend/src/main/java/com/twittvl/backend/tweet/TweetService.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetService.java
@@ -4,6 +4,8 @@ import com.twittvl.backend.common.exception.ResourceNotFoundException;
 import com.twittvl.backend.user.User;
 import com.twittvl.backend.user.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.beans.Transient;
@@ -33,6 +35,17 @@ public class TweetService {
 
         Tweet saved =  tweetRepository.save(tweet);
         return tweetMapper.tweetToTweetResponse(saved);
+    }
+
+    public TweetResponse getById(Long id) {
+        Tweet tweet = tweetRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Tweet not found" + id));
+        return tweetMapper.tweetToTweetResponse(tweet);
+    }
+
+    public Page<TweetResponse> getFeed(Pageable pageable) {
+        return tweetRepository.findAllByOrderByCreatedAtDesc(pageable)
+                .map(tweetMapper::tweetToTweetResponse);
     }
     private boolean isBlank(String content) {
         return content == null || content.isBlank();
