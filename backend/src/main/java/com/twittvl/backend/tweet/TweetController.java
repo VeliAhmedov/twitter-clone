ae20a0aad3d19d191e30dd4c02f1b51c788ea6d2
ExternalMirror <ahmedovveli.outlook.com>
2026-08-30 23:14:07 +0400

    postTweet controller added

--- backend/src/main/java/com/twittvl/backend/tweet/TweetController.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetController.java
@@ -1,4 +1,28 @@
 package com.twittvl.backend.tweet;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/tweets")
 public class TweetController {
+    private final TweetService tweetService;
+
+    public  TweetController(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TweetResponse> postTweet (
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestBody
+            @Valid TweetRequest tweetRequest) {
+        TweetResponse tweetResponse = tweetService.postTweet(userId, tweetRequest);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(tweetResponse);
+    }
+
 }
