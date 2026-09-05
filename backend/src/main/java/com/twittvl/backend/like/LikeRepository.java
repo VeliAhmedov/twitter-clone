0db1eb7cbe35d8b57fca6b1342353f68428d2d62
ExternalMirror <ahmedovveli@outlook.com>
2026-09-05 21:44:00 +0400

    added annotation on repo

--- backend/src/main/java/com/twittvl/backend/like/LikeRepository.java
+++ backend/src/main/java/com/twittvl/backend/like/LikeRepository.java
@@ -1,9 +1,11 @@
 package com.twittvl.backend.like;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByTweetIdAndUserId(Long tweetId, Long userId);
     boolean existsByTweetIdAndUserId(Long tweetId, Long userId);
