e742fcae16f0946a81ceca1ad67a9ead919d0d4b
ExternalMirror <ahmedovveli@outlook.com>
2026-09-05 23:24:01 +0400

    added like controller

--- backend/src/main/java/com/twittvl/backend/like/LikeController.java
+++ backend/src/main/java/com/twittvl/backend/like/LikeController.java
@@ -1,4 +1,26 @@
 package com.twittvl.backend.like;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/likes")
 public class LikeController {
+    private final LikeService likeService;
+    public LikeController(LikeService likeService) {this.likeService = likeService;}
+
+    @PostMapping
+    public long likeTweet(
+            @PathVariable long id,
+            @RequestHeader ("X-User-Id") Long userId){
+        return likeService.likeTweet(id, userId);
+    }
+
+    @DeleteMapping
+    public long unlikeTweet(
+            @PathVariable long id,
+            @RequestHeader ("X-User-Id") Long userId){
+        return likeService.unlikeTweet(id, userId);
+    }
+
+
 }
