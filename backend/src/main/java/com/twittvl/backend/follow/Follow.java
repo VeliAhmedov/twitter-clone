076aebe7a47109b4c7464975a14a5a3d4bcdde5f
ExternalMirror <ahmedovveli@outlook.com>
2026-09-06 23:23:02 +0400

    follow entity added

--- backend/src/main/java/com/twittvl/backend/follow/Follow.java
+++ backend/src/main/java/com/twittvl/backend/follow/Follow.java
@@ -1,4 +1,46 @@
 package com.twittvl.backend.follow;
 
+import com.twittvl.backend.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "follows")
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_id", nullable = false)
+    private User followed;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Follow follow)) return false;
+        return id != null && id.equals(follow.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
