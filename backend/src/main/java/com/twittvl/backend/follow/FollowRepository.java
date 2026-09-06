729227ae1dc539f87318e8b49c7fd1a3a1058183
ExternalMirror <ahmedovveli@outlook.com>
2026-09-06 23:31:02 +0400

    follow repo coded

--- backend/src/main/java/com/twittvl/backend/follow/FollowRepository.java
+++ backend/src/main/java/com/twittvl/backend/follow/FollowRepository.java
@@ -1,4 +1,11 @@
 package com.twittvl.backend.follow;
 
-public interface FollowRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, Integer> {
+    Optional<Follow> findByTweetIdAndUserId(Long tweetId, Long userId);
+    boolean existsByTweetIdAndUserId(Long tweetId, Long userId);
+    long countByTweetIdAndUserId(Long tweetId, Long userId);
 }
