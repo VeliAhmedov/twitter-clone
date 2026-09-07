0290e99a1846a75804b8c87bb87dd45dad06e23a
ExternalMirror <ahmedovveli@outlook.com>
2026-09-08 00:03:29 +0400

    comment entity added

--- backend/src/main/java/com/twittvl/backend/comment/Comment.java
+++ backend/src/main/java/com/twittvl/backend/comment/Comment.java
@@ -1,4 +1,62 @@
 package com.twittvl.backend.comment;
 
+import com.twittvl.backend.tweet.Tweet;
+import com.twittvl.backend.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table (name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tweet_id", nullable = false)
+    private Tweet tweet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 150)
+    private String content;
+
+    private String imageUrl;
+
+    private boolean edited = false;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comments)) return false;
+        return id != null && id.equals(comments.id);
+    }
+
+    @Override
+    public int hashCode() {return getClass().hashCode();}
 }
