f47f32724aebbe025cf068924c65627a09ae21b2
ExternalMirror <ahmedovveli@outlook.com>
2026-09-07 21:37:19 +0400

    follow controller done

--- backend/src/main/java/com/twittvl/backend/follow/FollowController.java
+++ backend/src/main/java/com/twittvl/backend/follow/FollowController.java
@@ -1,4 +1,26 @@
 package com.twittvl.backend.follow;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users/{followedId}/follow")
 public class FollowController {
+    private final FollowService followService;
+    public  FollowController(FollowService followService) {
+        this.followService = followService;
+    }
+
+    @PostMapping
+    public Long followUser(
+            @RequestHeader("X-User-Id") Long followerId,
+            @PathVariable Long followedId) {
+        return followService.followUser(followerId, followedId);
+    }
+
+    @DeleteMapping
+    public Long unfollowUser(
+            @RequestHeader("X-User-Id") Long followerId,
+            @PathVariable Long followedId) {
+        return followService.unfollowUser(followerId, followedId);
+    }
 }
