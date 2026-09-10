52bc7e15c41d4d99cd57d3e4379ab1cfd096f750
ExternalMirror <ahmedovveli@outlook.com>
2026-09-10 14:52:02 +0400

    added comment repo

--- backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.twittvl.backend.comment;
 
-public interface CommentRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findAllByTweetIdOrderByCreatedAtDesc(Long tweetId, Pageable pageable);
+    Page<Comment> findAllByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 }
