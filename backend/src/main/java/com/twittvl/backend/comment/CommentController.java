751d72cb151f8933f9820a0958d2900dbc70ade6
ExternalMirror <ahmedovveli@outlook.com>
2026-09-12 00:06:52 +0400

    create comment and getbyid controller api added

--- backend/src/main/java/com/twittvl/backend/comment/CommentController.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentController.java
@@ -1,4 +1,32 @@
 package com.twittvl.backend.comment;
 
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tweets/{tweetId}/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> createComment(
+            @RequestHeader("X-User-Id") Long userId,
+            @PathVariable Long tweetId,
+            @RequestBody @Valid CommentRequest commentRequest) {
+        CommentResponse commentResponse = commentService.createComment(commentRequest, userId, tweetId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentResponse);
+    }
+
+    @GetMapping("/{id}")
+    public CommentResponse getById (@PathVariable Long id) {
+        return commentService.getById(id);
+    }
 }
