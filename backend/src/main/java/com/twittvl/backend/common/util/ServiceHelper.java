7495675b754a80ef491ec54539f1ff210f27d58c
ExternalMirror <ahmedovveli@outlook.com>
2026-09-11 23:00:50 +0400

    global isBlank helper method added for services

---
+++ backend/src/main/java/com/twittvl/backend/common/util/ServiceHelper.java
@@ -0,0 +1,7 @@
+package com.twittvl.backend.common.util;
+
+public class ServiceHelper {
+    public static boolean isBlank(String content) {
+        return content == null || content.isBlank();
+    }
+}
