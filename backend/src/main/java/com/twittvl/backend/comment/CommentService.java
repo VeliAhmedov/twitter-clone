d5b616a6da0018f21fc70f0543e34dcb12ca116d
ExternalMirror <ahmedovveli@outlook.com>
2026-09-11 21:02:49 +0400

    comment service getbyid and bytweetid added

--- backend/src/main/java/com/twittvl/backend/comment/CommentService.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentService.java
@@ -5,6 +5,9 @@ import com.twittvl.backend.tweet.Tweet;
 import com.twittvl.backend.tweet.TweetRepository;
 import com.twittvl.backend.user.User;
 import com.twittvl.backend.user.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +17,8 @@ public class CommentService {
     private final UserRepository userRepository;
     private final TweetRepository tweetRepository;
     private final CommentRepository commentRepository;
-    public CommentService(CommentMapper commentMapper,CommentRepository commentRepository, TweetRepository tweetRepository, UserRepository userRepository) {
+
+    public CommentService(CommentMapper commentMapper, CommentRepository commentRepository, TweetRepository tweetRepository, UserRepository userRepository) {
         this.commentMapper = commentMapper;
         this.commentRepository = commentRepository;
         this.tweetRepository = tweetRepository;
@@ -23,7 +27,7 @@ public class CommentService {
 
     @Transactional()
     public CommentResponse createComment(CommentRequest commentRequest, Long userId, Long tweetId) {
-        if (isBlank(commentRequest.content()) && isBlank(commentRequest.url())){
+        if (isBlank(commentRequest.content()) && isBlank(commentRequest.url())) {
             throw new IllegalArgumentException("comment can't be empty");
         }
         User user = userRepository.findById(userId)
@@ -39,6 +43,19 @@ public class CommentService {
         Comment savedComment = commentRepository.save(comment);
         return commentMapper.toCommentResponse(savedComment);
 
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CommentResponse> getCommentsByTweedId(Pageable pageable, Long tweetId) {
+        return commentRepository.findAllByTweetIdOrderByCreatedAtDesc(tweetId, pageable)
+                .map(commentMapper::toCommentResponse);
+    }
+
+    @Transactional(readOnly = true)
+    public CommentResponse getById(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResourceNotFoundException("comment not found with id " + commentId));
+        return commentMapper.toCommentResponse(comment);
     }
 
     private boolean isBlank(String content) {
