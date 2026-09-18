7c85a22e1e7db20400e58c4daa3440d6dc05cbab
ExternalMirror <ahmedovveli@outlook.com>
2026-09-18 17:21:27 +0400

    commentlike repository interface added

--- backend/src/main/java/com/twittvl/backend/commentLike/commentLikeRepository.java
+++ backend/src/main/java/com/twittvl/backend/commentLike/commentLikeRepository.java
@@ -1,4 +1,13 @@
 package com.twittvl.backend.commentLike;
 
-public interface commentLikeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface commentLikeRepository extends JpaRepository<CommentLike, Long> {
+    Optional<CommentLike> findByCommentIdAndUserId(Long commentId, Long userId);
+    boolean existsByCommentIdAndUserId(Long commentId, Long userId);
+    long countByCommentId(Long commentId);
 }
