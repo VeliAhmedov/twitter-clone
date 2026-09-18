cbc4be1c7339892e9492314de2af3cdedf1620fb
ExternalMirror <ahmedovveli@outlook.com>
2026-09-18 23:36:56 +0400

    commentLike controller added

--- backend/src/main/java/com/twittvl/backend/commentLike/CommentLikeController.java
+++ backend/src/main/java/com/twittvl/backend/commentLike/CommentLikeController.java
@@ -1,4 +1,27 @@
 package com.twittvl.backend.commentLike;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/commentLikes")
 public class CommentLikeController {
+
+    private CommentLikeService commentLikeService;
+    public CommentLikeController(CommentLikeService commentLikeService) {
+        this.commentLikeService = commentLikeService;
+    }
+
+    @PostMapping
+    public long likeComment(
+            @PathVariable long commentId,
+            @RequestHeader("X-User-Id") Long userId){
+        return commentLikeService.likeComment(userId, commentId);
+    }
+
+    @DeleteMapping
+    public long unlikeComment(
+            @PathVariable long commentId,
+            @RequestHeader ("X-User-Id") Long userId){
+        return commentLikeService.unlikeComment(userId, commentId);
+    }
 }
