1ee2d9e01bc7f4470733b3a5f3137cadbd39280f
ExternalMirror <ahmedovveli@outlook.com>
2026-09-19 23:41:15 +0400

    get comment likers API controller added

--- backend/src/main/java/com/twittvl/backend/commentLike/CommentLikeController.java
+++ backend/src/main/java/com/twittvl/backend/commentLike/CommentLikeController.java
@@ -1,10 +1,13 @@
 package com.twittvl.backend.commentLike;
 
+import com.twittvl.backend.tweetLike.TweetLikeUserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommentLikeController {
-
     private final CommentLikeService commentLikeService;
     public CommentLikeController(CommentLikeService commentLikeService) {
         this.commentLikeService = commentLikeService;
@@ -22,5 +25,12 @@ public class CommentLikeController {
             @PathVariable Long commentId,
             @RequestHeader ("X-User-Id") Long userId){
         return commentLikeService.unlikeComment(userId, commentId);
+    }
+
+    @GetMapping("/api/comments/{commentId}/likes")
+    public Page<CommentLikeUserResponse> getCommentLikers(
+            @PathVariable Long commentId,
+            @PageableDefault(size = 20) Pageable pageable){
+        return commentLikeService.getCommentLikers(commentId, pageable);
     }
 }
