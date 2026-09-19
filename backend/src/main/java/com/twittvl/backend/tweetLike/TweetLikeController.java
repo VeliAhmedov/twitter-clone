9022a4a4f5f16d09bf85e07535fc923af056b6ef
ExternalMirror <ahmedovveli@outlook.com>
2026-09-19 18:37:22 +0400

    mapping refix in tweetLike controller

--- backend/src/main/java/com/twittvl/backend/tweetLike/TweetLikeController.java
+++ backend/src/main/java/com/twittvl/backend/tweetLike/TweetLikeController.java
@@ -3,21 +3,20 @@ package com.twittvl.backend.tweetLike;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/tweetLikes")
 public class TweetLikeController {
     private final TweetLikeService tweetLikeService;
     public TweetLikeController(TweetLikeService tweetLikeService) {this.tweetLikeService = tweetLikeService;}
 
-    @PostMapping("/{tweetId}")
+    @PostMapping("/api/tweets/{tweetId}/likes")
     public long likeTweet(
-            @PathVariable long tweetId,
+            @PathVariable Long tweetId,
             @RequestHeader ("X-User-Id") Long userId){
         return tweetLikeService.likeTweet(userId, tweetId);
     }
 
-    @DeleteMapping("/{tweetId}")
+    @DeleteMapping("/api/tweets/{tweetId}/likes")
     public long unlikeTweet(
-            @PathVariable long tweetId,
+            @PathVariable Long tweetId,
             @RequestHeader ("X-User-Id") Long userId){
         return tweetLikeService.unlikeTweet(userId, tweetId);
     }
