bb3adff06f9757d9321b45c3f950a098846e299e
ExternalMirror <ahmedovveli@outlook.com>
2026-09-20 15:02:16 +0400

    stere annoation fox in comment repo

--- backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
@@ -3,7 +3,9 @@ package com.twittvl.backend.comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByTweetIdOrderByCreatedAtDesc(Long tweetId, Pageable pageable);
     Page<Comment> findAllByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
