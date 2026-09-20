3dad6bc245d20561bd553d8495a0e871b0c1a1c3
ExternalMirror <ahmedovveli@outlook.com>
2026-09-20 15:01:48 +0400

    byparentcommentid repo added to comment repo

--- backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByTweetIdOrderByCreatedAtDesc(Long tweetId, Pageable pageable);
     Page<Comment> findAllByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+    Page<Comment> findAllByParentCommentIdOrderByCreatedAtDesc(Long parentCommentId, Pageable pageable);
 }
