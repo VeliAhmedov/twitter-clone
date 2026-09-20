877845cec1eb20fa62560c4cbe1e533f64f68c79
ExternalMirror <ahmedovveli@outlook.com>
2026-09-20 18:56:52 +0400

    parent comment on delete annoaion

--- backend/src/main/java/com/twittvl/backend/comment/Comment.java
+++ backend/src/main/java/com/twittvl/backend/comment/Comment.java
@@ -5,6 +5,8 @@ import com.twittvl.backend.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.Instant;
 
@@ -27,6 +29,7 @@ public class Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_comment_id")
+    @OnDelete(action = OnDeleteAction.SET_NULL)
     private Comment parentComment; //signify if null it is top comment if not null then comment on another comment
 
     @Column(length = 150)
