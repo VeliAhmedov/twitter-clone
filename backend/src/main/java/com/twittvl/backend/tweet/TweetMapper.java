179f0459fc00c8f165a1b5f3dea9184e0d7524a6
ExternalMirror <ahmedovveli@outlook.com>
2026-09-20 23:43:45 +0400

    likecount error ignore annoation

--- backend/src/main/java/com/twittvl/backend/tweet/TweetMapper.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetMapper.java
@@ -7,6 +7,7 @@ public interface TweetMapper {
     @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "username", source = "user.username")
     @Mapping(target = "userAvatarUrl", source = "user.avatarURL")
+    @Mapping(target = "likeCount", ignore = true)
     TweetResponse tweetToTweetResponse(Tweet tweet);
 
     void applyUpdate (TweetRequest tweetRequest, @MappingTarget Tweet tweet);
