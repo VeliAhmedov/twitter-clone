81c62d191c2bad9d58cb5d8a13a887f70b62d580
ExternalMirror <ahmedovveli@outlook.com>
2026-09-21 20:14:11 +0400

    email annoation

--- backend/src/main/java/com/twittvl/backend/user/CreateUserRequestTemp.java
+++ backend/src/main/java/com/twittvl/backend/user/CreateUserRequestTemp.java
@@ -1,5 +1,6 @@
 package com.twittvl.backend.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -16,6 +17,7 @@ public record CreateUserRequestTemp(
         @Size(min = 1, max = 30)
         String displayName,
         @NotBlank
+        @Email
         String email
 ) {
 }
