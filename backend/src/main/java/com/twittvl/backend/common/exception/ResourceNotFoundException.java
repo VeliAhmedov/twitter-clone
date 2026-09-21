149d449fa3b82273b04176fc6c24d9f1bb729937
ExternalMirror <ahmedovveli@outlook.com>
2026-09-21 12:11:42 +0400

    missing message parameter fix

--- backend/src/main/java/com/twittvl/backend/common/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/twittvl/backend/common/exception/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package com.twittvl.backend.common.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
-        super();
+        super(message);
     }
 }
