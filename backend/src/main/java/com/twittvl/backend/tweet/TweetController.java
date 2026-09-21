2d4210509ec2091f513c7f93c90cdba68097408f
ExternalMirror <ahmedovveli@outlook.com>
2026-09-21 16:59:25 +0400

    clean up unused imports

--- backend/src/main/java/com/twittvl/backend/tweet/TweetController.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetController.java
@@ -3,12 +3,9 @@ package com.twittvl.backend.tweet;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- backend/src/main/java/com/twittvl/backend/tweet/TweetRequest.java
+++ backend/src/main/java/com/twittvl/backend/tweet/TweetRequest.java
@@ -1,9 +1,6 @@
 package com.twittvl.backend.tweet;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 public record TweetRequest(
