03e1bef143a9efebfafe2d7b02bcf6b8326e23f1
ExternalMirror <ahmedovveli@outlook.com>
2026-09-21 17:51:43 +0400

    unused import cleanup

--- backend/src/main/java/com/twittvl/backend/follow/FollowMapper.java
+++ backend/src/main/java/com/twittvl/backend/follow/FollowMapper.java
@@ -2,7 +2,6 @@ package com.twittvl.backend.follow;
 
 import com.twittvl.backend.user.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface FollowMapper {
