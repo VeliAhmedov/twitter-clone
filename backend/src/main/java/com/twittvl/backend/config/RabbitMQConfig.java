9ca4c80fcc6581e15fcaf277433b4a7e09f95cca
ExternalMirror <ahmedovveli@outlook.com>
2026-09-23 09:53:14 +0400

    notification built

--- backend/src/main/java/com/twittvl/backend/config/RabbitMQConfig.java
+++ backend/src/main/java/com/twittvl/backend/config/RabbitMQConfig.java
@@ -1,4 +1,7 @@
 package com.twittvl.backend.config;
 
 public class RabbitMQConfig {
+    public static final String QUEUE_NAME = "my_queue";
+    public static final String EXCHANGE_NAME = "my_exchange";
+    public static final String ROUTING_KEY = "my_routing_key";
 }
--- backend/src/main/java/com/twittvl/backend/notification/NotificationProducer.java
+++ backend/src/main/java/com/twittvl/backend/notification/NotificationProducer.java
@@ -1,6 +1,13 @@
 package com.twittvl.backend.notification;
+
+import com.twittvl.backend.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
 //if I want being swappable transports,
 //I can change to interface +RabbitMQNotificationProducer implementation
 //Purpose: sending notification
+@Component
 public class NotificationProducer {
+
 }
