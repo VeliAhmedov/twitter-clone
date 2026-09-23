4fa5bed4329ef8c1a35b6ab70503aecd6b419a04
ExternalMirror <ahmedovveli@outlook.com>
2026-09-23 07:52:40 +0400

    notification response added

--- backend/src/main/java/com/twittvl/backend/notification/NotificationResponse.java
+++ backend/src/main/java/com/twittvl/backend/notification/NotificationResponse.java
@@ -1,4 +1,17 @@
 package com.twittvl.backend.notification;
+
+import java.time.Instant;
+
 //purpose: displaying notification as response
-public record NotificationResponse() {
+public record NotificationResponse(
+        Long id,
+        Long senderId,
+        String senderDisplayName,
+        NotificationType notificationType,
+        String message,
+        Long tweetId,
+        Long commentId,
+        boolean read,
+        Instant createdAt
+) {
 }
