07318ef545a5c2ab1d6e3378bc321c3578858112
ExternalMirror <ahmedovveli@outlook.com>
2026-09-23 23:14:27 +0400

    notificaiton controller added

--- backend/src/main/java/com/twittvl/backend/notification/NotificationController.java
+++ backend/src/main/java/com/twittvl/backend/notification/NotificationController.java
@@ -1,4 +1,39 @@
 package com.twittvl.backend.notification;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
+    private final NotificationService notificationService;
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping
+    public Page<NotificationResponse> getNotifications(
+            @RequestHeader("X-User-Id") Long userId,
+            @PageableDefault(size = 20)  Pageable pageable) {
+        return notificationService.getNotifications(userId, pageable);
+    }
+
+    @GetMapping("/unread-count")
+    public long getUnreadNotificationCount(@RequestHeader("X-User-Id") Long userId) {
+        return notificationService.getUnreadCount(userId);
+    }
+
+    @PatchMapping("/{notificationId}/read")
+    public NotificationResponse markAsRead(
+            @RequestHeader("X-User-Id") Long userId,
+            @PathVariable Long notificationId) {
+        return notificationService.markAsRead(userId, notificationId);
+    }
+
+    @PatchMapping("/read-all")
+    public int markAllAsRead(@RequestHeader("X-User-Id") Long userId) {
+        return notificationService.markAllAsRead(userId);
+    }
 }
