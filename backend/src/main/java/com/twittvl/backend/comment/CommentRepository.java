c63145b614aae21208948ec040ed4271adb6f7e2
ExternalMirror <ahmedovveli@outlook.com>
2026-09-23 21:57:10 +0400

    added mark all isread to true repo

--- backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
+++ backend/src/main/java/com/twittvl/backend/comment/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.twittvl.backend.comment;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +16,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByParentCommentIdOrderByCreatedAtDesc(Long parentCommentId, Pageable pageable);
     long countByTweetIdAndParentCommentIsNull(Long tweetId);
     long countByParentCommentId(Long parentCommentId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.recipient.id = :userId AND n.isRead = false")
+    int markAllAsReadByRecipientId(@Param("userId") Long userId);
 }
