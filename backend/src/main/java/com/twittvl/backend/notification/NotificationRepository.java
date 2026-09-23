0b78d5cb81bebe875de6f2e82f20b6f409e09cbb
ExternalMirror <ahmedovveli@outlook.com>
2026-09-23 21:59:17 +0400

    making all notification mark as read repo added

--- backend/src/main/java/com/twittvl/backend/notification/NotificationRepository.java
+++ backend/src/main/java/com/twittvl/backend/notification/NotificationRepository.java
@@ -1,11 +1,18 @@
 package com.twittvl.backend.notification;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findAllByRecipientIdOrderByCreatedAtDesc(Long recipientId, Pageable pageable);
 
     long countByRecipientIdAndIsReadFalse(Long recipientId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.recipient.id = :userId AND n.isRead = false")
+    int markAllAsReadByRecipientId(@Param("userId") Long userId);
 }
