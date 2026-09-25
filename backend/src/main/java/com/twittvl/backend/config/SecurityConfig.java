75e8f92ceb59aef59db094441c8ea037cff2e6c8
ExternalMirror <ahmedovveli@outlook.com>
2026-09-25 08:55:18 +0400

    argon2id password encoder added

--- backend/src/main/java/com/twittvl/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/twittvl/backend/config/SecurityConfig.java
@@ -1,9 +1,26 @@
 package com.twittvl.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password4j.Argon2Password4jPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class SecurityConfig {
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        String encodingId = "argon2id";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("argon2id", new Argon2Password4jPasswordEncoder());
+        encoders.put("bcrypt", new BCryptPasswordEncoder(12));
+        return new DelegatingPasswordEncoder(encodingId, encoders);
+    }
 }
