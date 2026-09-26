d75f0768c907105cb7b676b920d5adc4789b6f39
ExternalMirror <ahmedovveli@outlook.com>
2026-09-26 15:31:37 +0400

    jwtauthfilter added

--- backend/src/main/java/com/twittvl/backend/auth/AuthResponse.java
+++ backend/src/main/java/com/twittvl/backend/auth/AuthResponse.java
@@ -2,6 +2,6 @@ package com.twittvl.backend.auth;
 
 public record AuthResponse(String accessToken, String refreshToken, String tokenType) {
     public AuthResponse(String accessToken, String refreshToken) {
-        this(accessToken, refreshToken, "tokenBearer");
+        this(accessToken, refreshToken, "Bearer");
     }
 }
