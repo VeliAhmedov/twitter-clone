2b7b1dbaa88bf7828b9559d47047e5f0ec98b30b
ExternalMirror <ahmedovveli@outlook.com>
2026-09-26 16:41:56 +0400

    hwt accessdenied handler added

---
+++ backend/src/main/java/com/twittvl/backend/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.twittvl.backend.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twittvl.backend.common.exception.ApiErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.time.Instant;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ApiErrorResponse forbiddenError = new ApiErrorResponse(
+                HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase(),
+                "You don't have permission to access to this source", request.getRequestURI(), Instant.now());
+        response.getWriter().write(mapper.writeValueAsString(forbiddenError));
+    }
+}
