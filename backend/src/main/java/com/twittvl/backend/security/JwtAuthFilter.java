c871ea8deb44fc50b2a584edd04f2d5987e8ab26
ExternalMirror <ahmedovveli@outlook.com>
2026-09-26 15:32:16 +0400

    jwtauthfilter implemented

--- backend/src/main/java/com/twittvl/backend/security/JwtAuthFilter.java
+++ backend/src/main/java/com/twittvl/backend/security/JwtAuthFilter.java
@@ -1,16 +1,49 @@
 package com.twittvl.backend.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+    public JwtAuthFilter(JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                Claims claims = jwtUtil.parseAndValidateAccessToken(token);
+                if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }catch (JwtException | IllegalArgumentException ex) {
+                SecurityContextHolder.clearContext(); // if it is bad or expired token, continue unauthenticated
+            }
+        }
+        filterChain.doFilter(request, response); //always continue, filter will still auth process, never reject
     }
 }
+// will check in every request if request has Authorization : Bearer token
+// Telling spring, it is authenticated and here is who
